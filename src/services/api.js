ba4b0fb4f71345603d149b2f06357802fc860a89
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-14 15:32:36 -0300

    Correcao na baseURL do axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000' //local
-  baseURL: 'https://api-devclub-cadastros-usuarios.onrender.com/usuarios'
+  baseURL: 'https://api-devclub-cadastros-usuarios.onrender.com/'
 });
 
 export default api;
