05589101e9588548460d298b28f5e33a4342adee
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-15 18:53:33 -0300

    Mudando a baseURL para evitar undefined

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,23 @@
 import axios from 'axios';
 
-const baseURL = (() => {
-  // Vercel environment
-  if (import.meta.env.VERCEL_ENV) {
-    if (import.meta.env.VERCEL_ENV === 'production') {
-      return import.meta.env.VITE_PRODUCTION_API_URL || `https://${import.meta.env.VERCEL_URL}`;
+const getBaseURL = () => {
+   // Vercel environment
+  const { VERCEL_ENV, VERCEL_URL, VERCEL_BRANCH_URL, VITE_PRODUCTION_API_URL, VITE_LOCAL_API_URL } = import.meta.env;
+
+  if (VERCEL_ENV) {
+    if (VERCEL_ENV === 'production') {
+      return VITE_PRODUCTION_API_URL || (VERCEL_URL ? `https://${VERCEL_URL}` : '');
     }
-    if (import.meta.env.VERCEL_ENV === 'preview') {
-      return `https://${import.meta.env.VERCEL_BRANCH_URL || import.meta.env.VERCEL_URL}`;
+    if (VERCEL_ENV === 'preview') {
+      return VERCEL_BRANCH_URL ? `https://${VERCEL_BRANCH_URL}` : (VERCEL_URL ? `https://${VERCEL_URL}` : '');
     }
   }
   // Local environment (development)
-  return import.meta.env.VITE_LOCAL_API_URL || 'http://localhost:3000';
-})();
+  return VITE_LOCAL_API_URL || 'http://localhost:3000';
+};
 
 const api = axios.create({
-  baseURL,
+  baseURL: getBaseURL(),
   headers: {
     'Content-Type': 'application/json',
   }
