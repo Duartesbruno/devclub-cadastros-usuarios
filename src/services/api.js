8e348d146a5022bf8edb2a425e3826be80883def
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-15 18:06:44 -0300

    reestruturação baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api = axios.create({
-  // baseURL: 'http://localhost:3000' //local
-  baseURL: 'https://api-devclub-cadastros-usuarios.onrender.com/'
+const baseURL =
+  import.meta.env.VITE_PRODUCTION_API_URL && import.meta.env.MODE === 'production'
+    ? import.meta.env.VITE_PRODUCTION_API_URL
+    : import.meta.env.VITE_LOCAL_API_URL || 'http://localhost:3000';
+
+const api = axios.create({ 
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 export default api;
