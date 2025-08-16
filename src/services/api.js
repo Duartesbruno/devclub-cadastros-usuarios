928ccf8968c37748b47517628233c990ecb90dc0
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-16 02:31:12 -0300

    ajuste para teste no getBaseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,21 @@
 import axios from 'axios';
 
 const getBaseURL = () => {
-  const isProduction = import.meta.env.PROD; // true em produção, false em desenvolvimento/preview
-  const VITE_LOCAL_API_URL = import.meta.env.VITE_LOCAL_API_URL;
+  // const isProduction = import.meta.env.PROD;
   const VITE_PRODUCTION_API_URL = import.meta.env.VITE_PRODUCTION_API_URL;
-  const VITE_VERCEL_URL = import.meta.env.VITE_VERCEL_URL;
-  const VITE_VERCEL_BRANCH_URL = import.meta.env.VITE_VERCEL_BRANCH_URL;
+  // const VITE_HML_API_URL = import.meta.env.VITE_HML_API_URL;
+  const VITE_LOCAL_API_URL = import.meta.env.VITE_LOCAL_API_URL;
 
-  if (isProduction && VITE_PRODUCTION_API_URL) {
+  // if (isProduction && VITE_PRODUCTION_API_URL) {
+  //   return VITE_PRODUCTION_API_URL;
+  // }
+
+  // if (!isProduction && VITE_HML_API_URL) {
+  //   return VITE_HML_API_URL;
+  // }
+
+  if (VITE_PRODUCTION_API_URL) {
     return VITE_PRODUCTION_API_URL;
-  }
-
-  if (!isProduction && VITE_VERCEL_BRANCH_URL) {
-    return `https://${VITE_VERCEL_BRANCH_URL}`;
-  }
-
-  if (!isProduction && VITE_VERCEL_URL) {
-    return `https://${VITE_VERCEL_URL}`;
   }
 
   return VITE_LOCAL_API_URL || 'http://localhost:3000';
