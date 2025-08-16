3f226b28df853c8cc63a814a2136960e91d47761
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-16 01:58:48 -0300

    Ajustes para arrumar validacao getBaseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 
 const getBaseURL = () => {
+  const isProduction = import.meta.env.PROD; // true em produção, false em desenvolvimento/preview
   const VITE_LOCAL_API_URL = import.meta.env.VITE_LOCAL_API_URL;
   const VITE_PRODUCTION_API_URL = import.meta.env.VITE_PRODUCTION_API_URL;
   const VITE_VERCEL_URL = import.meta.env.VITE_VERCEL_URL;
   const VITE_VERCEL_BRANCH_URL = import.meta.env.VITE_VERCEL_BRANCH_URL;
 
-  // Vercel Production
-  if (VITE_PRODUCTION_API_URL) return VITE_PRODUCTION_API_URL;
+  if (isProduction && VITE_PRODUCTION_API_URL) {
+    return VITE_PRODUCTION_API_URL;
+  }
 
-  // Preview / Workspace Vercel
-  if (VITE_VERCEL_BRANCH_URL) return `https://${VITE_VERCEL_BRANCH_URL}`;
-  if (VITE_VERCEL_URL) return `https://${VITE_VERCEL_URL}`;
+  if (!isProduction && VITE_VERCEL_BRANCH_URL) {
+    return `https://${VITE_VERCEL_BRANCH_URL}`;
+  }
 
-  // Local / fallback
+  if (!isProduction && VITE_VERCEL_URL) {
+    return `https://${VITE_VERCEL_URL}`;
+  }
+
   return VITE_LOCAL_API_URL || 'http://localhost:3000';
 };
 
